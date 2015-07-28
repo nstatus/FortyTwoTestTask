a903b11d49d80d9325f4dc443ec6dfbfb1193224
sergey <sergey.sipiev@yandex.ru>
2015-07-28 21:27:35 +0300

    ticket:1 test pass

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,9 +1,7 @@
-from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from apps.hello.models import UserProfile
 
 
 def contacts(request):
-    # user_data = UserProfile.objects.all().first()
-    # return render(request, 'contacts.html', {'user': user_data})
-    return HttpResponse(status=404)
+    user_data = UserProfile.objects.all().first()
+    return render(request, 'contacts.html', {'user': user_data})
