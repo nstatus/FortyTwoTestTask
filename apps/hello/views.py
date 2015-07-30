aeeb48553a36f6f316e94ce6249c0224c0349dcc
sergey <sergey.sipiev@yandex.ru>
2015-07-30 14:39:04 +0300

    ticket:1 checking whether fixture is not implemented

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from apps.hello.models import UserProfile
 
 
 def contacts(request):
     user_data = UserProfile.objects.all().first()
+    if not user_data:
+        return HttpResponse('User not found')
     return render(request, 'contacts.html', {'user': user_data})
